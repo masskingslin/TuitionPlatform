c4db6d37fc07204b8dec44acb5818d906e394bab
Mass Kingslin <masskingslin@gmail.com>
2026-04-18 04:41:26 +0530

    Create MainActivity.kt

---
+++ app-student/src/main/java/com/tuition/student/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tuition.student
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val textView = TextView(this).apply { text = "Student App: Find a Tutor" }
+        setContentView(textView)
+    }
+}
