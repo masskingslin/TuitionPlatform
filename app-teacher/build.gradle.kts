84c05110e76d41f30291ae3f86c60b942a098a0b
Mass Kingslin <masskingslin@gmail.com>
2026-04-18 06:17:10 +0530

    Update build.gradle.kts

--- app-teacher/build.gradle.kts
+++ app-teacher/build.gradle.kts
@@ -2,14 +2,25 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
 }
+
 android {
     namespace = "com.tuition.teacher"
     compileSdk = 34
+    
     defaultConfig {
         applicationId = "com.tuition.teacher"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
